62fd780528e5db7b904d79a8581bb716fa9b0c6f
Derek.Fung <mewfung@gmail.com>
2026-06-30 01:56:55 +0800

    fix packaging

--- build.gradle.kts
+++ build.gradle.kts
@@ -464,7 +464,7 @@ fun githubReleaseArtifacts(config: GithubReleaseConfig): List<GithubReleaseArtif
         GithubReleaseArtifact(
             shadowJarFile,
             "application/java-archive",
-            "${project.name}-update-${config.version}.jar"
+            "${project.name}-jpackage-update-${config.version}.jar"
         )
     )
 }
