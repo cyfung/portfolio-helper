c1885e182cac767c2c2dfeae89481ca9b3ed558f
Derek.Fung <mewfung@gmail.com>
2026-09-15 03:49:13 +0800

    android version 1.0.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.portfoliohelper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.6"
+        versionCode = 9
+        versionName = "1.0.7"
     }
 
     signingConfigs {
